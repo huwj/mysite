330a6caa7ce462e76ff53036bc09b9df604d275e
gdc <gdc@django.(none)>
2013-10-09 17:52:32 -0400

    add new app with dynamic content. Set up the connection between content and url

--- mysiteCODE/urls.py
+++ mysiteCODE/urls.py
@@ -4,12 +4,16 @@ import controllers
 # from django.contrib import admin
 # admin.autodiscover()
 
+from timer.views import hello, current_datetime, hours_ahead
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysiteCODE.views.home', name='home'),
     url(r'^$', controllers.index),
     # url(r'^mysiteCODE/', include('mysiteCODE.foo.urls')),
-
+    url(r'^hello/$', hello), #timer.views.hello    
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d+)/$', hours_ahead),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
