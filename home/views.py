f4e792a1cd50652e949c6272d4722ce32c239fa4
gdc <gdc@django.(none)>
2013-10-21 17:09:09 -0400

    Implement login/out authentication mechanism. Using contrib appwq

---
+++ home/views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    return render(request, 'home.html')
--- mysiteCODE/urls.py
+++ mysiteCODE/urls.py
@@ -5,7 +5,7 @@ import controllers
 # admin.autodiscover()
 
 from timer.views import hello, current_datetime, hours_ahead
-
+from home.views import home
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysiteCODE.views.home', name='home'),
@@ -14,6 +14,9 @@ urlpatterns = patterns('',
     url(r'^hello/$', hello, name="hello"), #timer.views.hello    
     url(r'^time/$', current_datetime,name="timer"),
     url(r'^time/plus/(\d+)/$', hours_ahead, name="ahead"),
+    url(r'^home/$', home, name="home"),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name':'login.html'}),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name':'logout.html'}),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
